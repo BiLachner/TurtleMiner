a49046498f586be61b25f040ac3a224777c5a5e8
octacian <enduffy2014@outlook.com>
2016-12-01 18:07:14 -0800

    logger

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 turtleminer = {}
 turtleminer.modpath = minetest.get_modpath("turtleminer")
 
+-- logger
+function turtleminer.log(content, log_type)
+  if log_type == nil then log_type = "action" end
+  minetest.log(log_type, "[TurtleMiner] "..content)
+end
+
 -- load turtle resources
 dofile(turtleminer.modpath.."/t_api.lua") -- load turtle api
 dofile(turtleminer.modpath.."/turtles.lua") -- turtle register
